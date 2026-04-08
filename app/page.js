9e7da51dfbebbc7305fcb0ad019440d62b19c458
Atharva45 <atharvaphanse403@gmail.com>
2026-04-08 21:13:48 +0530

    integrated stream

--- app/page.js
+++ app/page.js
@@ -1,15 +1,32 @@
 "use client"
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  useState("");
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+
+  const handleJoin = () => {
+    const name = username.trim() === "" ? "Guest" : username.trim();
+    
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
       <div className= "p-8 bg-gray-800/60 rounded-2xl border border-gray-700 w-80 shadow-2xl">
       <h2 className="text-xl font-semibold mb-4 text-center">Enter your Name</h2>
 
-      <input className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray-600 text-white"/>
+      <input 
+      placeholder="eg. Atharva" 
+      className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray-600 text-white"
+      value = {username}
+      onChange = {(e) => setUsername(e.target.value)}
+      />
+
+      <button 
+      onClick={handleJoin}
+      className="mt-5 w-full py-3 bg-blue-600 hover:bg-blue-700 rounded-lg font-medium">Join Meeting</button>
       </div>
     </div>
   );
